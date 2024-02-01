0dcab73da0667d8bd441959c5f199c4985281513
clementbrandenbourger <brandenbourger.c@sfeir.com>
2024-02-01 14:02:23 +0100

    Add GitHub actions - Upload firebase config file

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration - Will be replaced by Actions
+const firebaseConfig = ##FIREBASE_CONFIG##
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
